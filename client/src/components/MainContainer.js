7a7fcd5bf758214cf9bc59f3eb3f47b6392fc094
toon <nu.chai56@gmail.com>
2020-05-16 00:40:39 -0500

    test heroku

---
+++ client/src/components/MainContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+// import API from "../utils/API";
+import '../styles/style.css';
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="header">
+                    <h1>Employee Directory</h1>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+
+export default MainContainer;
