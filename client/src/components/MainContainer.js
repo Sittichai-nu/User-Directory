98245cf514a493a6dba94f689c1054df8eb65f9f
toon <nu.chai56@gmail.com>
2020-05-24 14:41:58 -0500

    fixed table

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -1,88 +1,67 @@
 import React, { Component } from "react";
-import Card from './Card';
-import SearchForm from './SearchForm';
+import Table from "./Table";
+import SearchForm from "./SearchForm";
 import API from "../utils/API";
 class MainContainer extends Component {
-    state = {
-        result: [],
-        filter: "",
-        filterBy: "lastName",
-        currentSort: "default",
-        sortField: ""
-    }
-    componentDidMount() {
-        API.search()
-            .then(res => {
-                console.log(res)
-                this.setState({
-                    result: res.data.results.map((e, i) => ({
-                        firstName: e.name.first,
-                        lastName: e.name.last,
-                        picture: e.picture.large,
-                        email: e.email,
-                        phone: e.phone,
-                        dob: e.date,
-                        key: i
-                    }))
-                })
-            })
-            .catch(err => console.log(err));
-    }
-    filterEmployees = (searchkey) => {
-        var filterResult = this.state.result.filter(person => person.firstName === searchkey)
-
+  state = {
+    result: [],
+  };
+  componentDidMount() {
+    API.search()
+      .then((res) => {
+        console.log(res);
         this.setState({
-            result: filterResult
-        })
-    }
-    handleFormSubmit = event => {
-        event.preventDefault();
-        const value = event.target.value;
-        const name = event.target.name;
-        this.filterEmployees(value);
-        this.setState({
-            [name]: value
+          result: res.data.results.map((e, i) => ({
+            firstName: e.name.first,
+            lastName: e.name.last,
+            picture: e.picture.large,
+            email: e.email,
+            phone: e.phone,
+            dob: e.date,
+            key: i,
+          })),
         });
-        this.filterEmployees(value);
-        this.filterEmployees(this.state.search);
+      })
+      .catch((err) => console.log(err));
+  }
+  filterEmployees = (searchkey) => {
+    var filterResult = this.state.result.filter(
+      (person) => person.firstName.includes(searchkey)
+    );
 
-    };
-    handleInputChange = event => {
-        event.preventDefault();
-        const value = event.target.value;
-        const name = event.target.name;
-        this.setState({
-            [name]: value
-
-        });
-
-    };
-    render() {
-        return (
-            <div className="container">
-                <div className="searching">
-                    <SearchForm
-                        value={this.state.search}
-                        handleInputChange={this.handleInputChange}
-                        handleFormSubmit={this.handleFormSubmit}
-                    />
-                </div>
-                <div>
-                    {[...this.state.result].map((item) =>
-                        <Card
-                            picture={item.picture}
-                            firstName={item.firstName}
-                            lastName={item.lastName}
-                            email={item.email}
-                            phone={item.phone}
-                            dob= {item.date}
-
-                            key={item.key}
-                        />
-                    )}
-                </div>
-            </div>
-        )
-    }
+    this.setState({
+      result: filterResult,
+    });
+  };
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.filterEmployees(this.state.search);
+  };
+  handleInputChange = (event) => {
+      console.log(event.target)
+    event.preventDefault();
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+  render() {
+    return (
+      <div className='container'>
+        <div className='searching'>
+          <SearchForm
+            value={this.state.search}
+            handleInputChange={this.handleInputChange}
+            handleFormSubmit={this.handleFormSubmit}
+          />
+        </div>
+        <div>
+          <Table 
+          result={this.state.result} />
+        </div>
+      </div>
+    );
+  }
 }
 export default MainContainer;
---
+++ client/src/components/Table.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../styles/style.css";
+
+function Table(props) {
+  return (
+    <table className='table'>
+      <thead className='nav'>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.result.map(item=>{
+           return(
+            <tr key={item.key}>
+            
+            <th className='col'>
+              <img alt={item.firstName} src={item.picture} />
+            </th>
+            <td>
+              {item.firstName} {item.lastName}
+            </td>
+            <td>{item.email}</td>
+            <td>{item.phone}</td>
+          </tr>
+           )
+          })
+        }
+        
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
